3a0c10b4607aa700917fca32f78e63e068e8d005
TRAVOR SSETTUBA <p1118231@tees.ac.uk>
2024-12-31 18:44:16 +0000

    add auth0

--- inventory/obj/Debug/net9.0/inventory.AssemblyInfo.cs
+++ inventory/obj/Debug/net9.0/inventory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("inventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efb499a5083c52110703d92314f6cb98ae6b87f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("inventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("inventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
